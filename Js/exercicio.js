78f2f66b9bb3316fe491f6b103f179dac5032af7
João Pedro <103079429+JoonhPedro@users.noreply.github.com>
2023-04-02 23:30:11 -0300

    Exercicio de Js

---
+++ Js/exercicio.js
@@ -0,0 +1,13 @@
+function calcMédia(notas) {
+    let notas1 = notas[0]
+    let notas2 = notas[1]
+    let notas3 = notas[2]
+    let media = (notas1 + notas2 + notas3)/3;
+    if(media>=7 && notas1>3 && notas2>3 && notas3>3){
+      console.log("parabens você foi Aprovado");
+    }else {
+      console.log ("infelizmente você nao foi Aprovado");
+    }
+  }
+  calcMédia([8,7,10]);
+   
